69f544543facd5681b8dc7c52daf4843517b992d
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-08-16 00:48:52 +0100

    added the header file + gitignore file

---
+++ shell.h
@@ -0,0 +1,11 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+
+#endif /* SHELL_H */
