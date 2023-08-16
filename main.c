501ed8a8838dcae48a2ea0cb762117b69088753c
codejunkie312 <119118398+codejunkie312@users.noreply.github.com>
2023-08-16 20:20:39 +0100

    handle arguments in the interperter

--- main.c
+++ main.c
@@ -8,32 +8,38 @@ int main(void)
 {
 	char command[100];
 	char *path[] = {NULL};
-	char *argv[2];
+	char *argv[100];
+	char *token;
+	int i;
 	pid_t pid;
 
 	argv[1] = NULL;
-
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
 			printf("($) ");
 		if (fgets(command, sizeof(command), stdin) == NULL)
-		{
 			break;
-		}
 
 		command[strcspn(command, "\n")] = 0;
 		if (strcmp(command, "exit") == 0)
-		{
 			break;
+
+		token = strtok(command, " ");
+		i = 0;
+		while (token != NULL)
+		{
+			argv[i] = token;
+			i++;
+			token = strtok(NULL, " ");
 		}
 
-		argv[0] = command;
+		argv[i] = NULL;
 
 		pid = fork();
 		if (pid == 0)
 		{
-			if (execve(command, argv, path) == -1)
+			if (execve(argv[0], argv, path) == -1)
 			{
 				fprintf(stderr, "./shell: No such file or directory\n");
 				return (1);
